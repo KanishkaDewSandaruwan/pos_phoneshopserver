24ad7013c1fc623d3d9bc05fd1735bb0b785210c
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-06-17 17:32:46 +0530

    add header access

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-    const { connection, config } = require('./config/connection');
-    const bodyParser = require('body-parser');
-    const express = require('express');
-    const app = express();
-    const routeHandler = require('./src/routes/index');
+const { connection, config } = require('./config/connection');
+const express = require('express');
+const app = express();
+const routeHandler = require('./src/routes/index');
+const { checkTables } = require('./config/table');
 
-    const { checkTables } = require('./config/table');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-    app.use('/api', routeHandler(config));
+app.use('/api', routeHandler(config));
 
-    app.all('*', (req, res) => {
-        res.status(404).send({
-            error: 'resource not found',
-        });
-    });
+app.all('*', (req, res) => {
+  res.status(404).send({
+    error: 'resource not found',
+  });
+});
 
-    const server = app.listen(config.port, () => {
-        console.log(`Server running at http://${config.hostname}:${server.address().port}/`);
-        checkTables();
-    });
+const server = app.listen(config.port, () => {
+  console.log(`Server running at http://${config.hostname}:${server.address().port}/`);
+  checkTables();
+});
