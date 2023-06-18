8bfcecdbd7b319509926ab60c35f79160c73a350
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-06-18 13:54:42 +0530

    up

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
+  next(); // Call next() to pass control to the next middleware or route handler
 });
 
 app.use('/api', routeHandler(config));
