8ccb9ef1a9ae871a4bc7cc812cc4ba1146157774
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-06-18 13:47:47 +0530

    xtoken acess

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const { checkTables } = require('./config/table');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
 });
 
 app.use('/api', routeHandler(config));
