0e144145d04f67883b67c808186776e622e0084a
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-07-03 22:56:09 +0530

    up create

--- src/routes/shop/shop.js
+++ src/routes/shop/shop.js
@@ -12,7 +12,7 @@ const { authenticateToken } = require('../../middlewares/userAuth');
 module.exports = (config) => {
     const router = express.Router();
 
-    // router.post('/create', authenticateToken, addShop);
+    router.post('/create', authenticateToken, addShop);
     router.get('/all', authenticateToken, getAllShops);
     router.get('/:shopId', authenticateToken, getShopById);
     router.put('/delete/:shopId', authenticateToken, deleteShop);
