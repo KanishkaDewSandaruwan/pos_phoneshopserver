26dc34c016b08ecb4b991a72d9e05d3f3913dcd4
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-02 11:47:25 +0530

    update

--- src/views/PermissionGroupView.js
+++ src/views/PermissionGroupView.js
@@ -1,6 +1,6 @@
 const permissionGroupView = {
     renderPermission(res, permission_group) { // <-- Make sure 'permission_group' is valid
-        console.log(permission_group)
+
         const { permission_code, assignpermissionid } = permission_group;
 
         const data = {
