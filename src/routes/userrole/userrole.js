c6cd81d0f1582eef16ef75cce09a947f87bc7c8e
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-23 09:04:23 +0530

    up

--- src/routes/userrole/userrole.js
+++ src/routes/userrole/userrole.js
@@ -16,7 +16,7 @@ const { authorizeAccessSupoerAdmin } = require('../../middlewares/userAccess');
 module.exports = (config) => {
   const router = express.Router();
 
-  router.post('/permissionByroleid/:userid', authenticateToken, permissionByroleid);
+  router.get('/permissionByroleid/:userid', authenticateToken, permissionByroleid);
 
   //super admin only
   router.post('/create', authorizeAccessSupoerAdmin, addUserRole);
